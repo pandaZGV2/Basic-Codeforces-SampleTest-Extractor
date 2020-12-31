4e6765ac279218c2e504406991b4b3c0fb39018b
PandaZGV2 <aaronmonis22@gmail.com>
2020-12-31 12:54:17 +0400

    Removed debuggers

--- cftestcase.py
+++ cftestcase.py
@@ -1,26 +1,23 @@
 import sys
 import json
-import urllib.request,urllib.parse,urllib.error
+import urllib.request
+import urllib.parse
+import urllib.error
 from html.parser import HTMLParser
 from bs4 import BeautifulSoup
 import re
 
-contestno=re.findall("[0-9]+",sys.argv[1])
-qno=re.findall("^[A-Z]",sys.argv[2])
-# print(contestno, qno)
-testno="1469"
-testletter="D"
-CFURL = "http://codeforces.com/contest/" + contestno[0] +"/problem/"+ qno[0]
+contestno = re.findall("[0-9]+", sys.argv[1])
+qno = re.findall("^[A-Z]", sys.argv[2])
+testno = "1469"
+testletter = "D"
+CFURL = "http://codeforces.com/contest/" + contestno[0] + "/problem/" + qno[0]
 html = urllib.request.urlopen(CFURL).read()
-soup =BeautifulSoup(html,'html.parser')
-div=soup.find_all('div', class_="input")
-f = open("input.txt","w")
+soup = BeautifulSoup(html, 'html.parser')
+div = soup.find_all('div', class_="input")
+f = open("input.txt", "w")
 for i in div:
-    a=i.find('pre')
+    a = i.find('pre')
     if a:
         f.write(a.text.strip())
-f.close()        
-# print(div)
-# print(Problem_Info)
-
-
+f.close()
