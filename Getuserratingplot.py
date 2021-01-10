3a6371e54779b256410613c7e1dbf5112a2c64ba
PandaZGV2 <aaronmonis22@gmail.com>
2021-01-10 12:49:02 +0400

    Changes to graph

--- Getuserratingplot.py
+++ Getuserratingplot.py
@@ -16,6 +16,9 @@ params = {'handle': username}
 details = urllib.parse.urlencode(params)
 url = 'https://codeforces.com/api/user.rating?'
 try:
+    # TRY REQUESTING INFORMATION FROM THE CODEFORCES API
+    # IN THE EVENT OF AN ERROR (EXAMPLE HTTP 404 NOT FOUND) GO TO EXCEPT CODE BLOCK AND EXIT THE PROGRAM
+
     finalurl = url+details
     encodeddata = urllib.request.urlopen(finalurl)
     data = encodeddata.read().decode()
@@ -24,9 +27,11 @@ except:
     print("Unable to read")
     exit()
 if jsondata['status'] == 'OK':
+    # CHECK IF THE DATA RECEIVED HAS 'OK' STATUS, IF NOT EXIT THE PROGRAM.
+
     print("Recieved handles successfully.")
     json_formatted_str = json.dumps(jsondata, indent=2)
-    print(json_formatted_str)
+    # print(json_formatted_str)
 else:
     print("Handle not recieved.\nPlease check the handle name.")
     exit()
@@ -36,26 +41,32 @@ contests = {}
 y = list()
 x = list()
 xax = list()
+# READ X-AXIS AND Y-AXIS DETAILS
+
 for contest in jsondata['result']:
     y.append(contest['newRating'])
     x.append(math.floor((contest['ratingUpdateTimeSeconds'] -
                          jsondata['result'][0]['ratingUpdateTimeSeconds'])/86400))
     xax.append(str(contest['contestId']))
-print(len(x))
-print(len(y))
+# print(len(x))
+# print(len(y))
 
 
-#
+# SET PLOT STYLES (SUBJECT TO CHANGE)
+
 fig = plt.figure()
 plt.title(username+" - Rating plot")
-plt.plot(x, y, 'c-o', markerfacecolor='r',markersize="10")
-# plt.xlim([-3, x[-1]])
+plt.plot(x, y, 'r-o', markerfacecolor='b', markersize="7", linewidth="2")
+plt.ylim([0, 4000])
 
-bars = plt.bar(x, y, color=(0.2, 0.4, 0.6, 0.1))
-print(len(bars))
+# SET BAR COLORS AND STYLES
+
+bars = plt.bar(x, y, color=(0.2, 0.4, 0.6, 0.05))
+# print(len(bars))
 plt.grid(True)
 contest = dict()
 
+# READ THE NECESSARY DETAILS FROM JSONDATA TO PLOT THE GRAPH
 
 def readelements():
     i = 0
@@ -68,21 +79,41 @@ def readelements():
                             'change': contest['newRating']-contest['oldRating'], 'Rating': contest['newRating']}
         i = i+1
 
+
         # contests['contest'].append(contest['contestName'])
         # contests['rank'].append(contest['rank'])
         # contests['change'].append(contest['newRating']-contest['oldRating'])
 readelements()
 yax = [1200, 1400, 1600, 1900, 2100, 2300, 2400, 2600, 3000, 4000]
-
+# plt.semilogy(yax)
 ax = plt.subplot()
+ax.set_yticks(yax)
 # plt.grid(color='#cccccc')
+ax.set_xticklabels([])
 # ax.set_xticklabels(xax, rotation='20')
 
+# SET ANNOTATION STYLES
+
 annot = ax.annotate("", xy=(0, 0), xytext=(-20, 20), textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="red", ec="b", lw=2),
+                    bbox=dict(boxstyle="round", fc=(1, 1, 0), ec="r", lw=2),
                     arrowprops=dict(arrowstyle="-|>"))
 annot.set_visible(False)
 
+# SET THE BACKGROUND COLOR FOR THE GRAPH
+
+ax.axhspan(0, 1200, facecolor=('#CCCCCC'), alpha=0.75)
+ax.axhspan(1200, 1400, facecolor='lime', alpha=0.75)
+ax.axhspan(1400, 1600, facecolor='cyan', alpha=0.75)
+ax.axhspan(1600, 1900, facecolor='indigo', alpha=0.75)
+ax.axhspan(1900, 2100, facecolor='purple', alpha=0.75)
+ax.axhspan(1900, 2100, facecolor='purple', alpha=0.75)
+ax.axhspan(2100, 2300, facecolor=(1, 1, 0), alpha=0.5)
+ax.axhspan(2300, 2400, facecolor=(1, 1, 0), alpha=0.75)
+ax.axhspan(2400, 2600, facecolor='red', alpha=0.35)
+ax.axhspan(2600, 3000, facecolor='red', alpha=0.75)
+ax.axhspan(3000, 4000, facecolor='red', alpha=1)
+
+# SET ANNOTATION DETAILS
 
 def update_annot(bar):
     x = bar.get_x()+bar.get_width()/2.
@@ -97,6 +128,7 @@ def update_annot(bar):
     annot.set_text(text)
     annot.get_bbox_patch().set_alpha(0.4)
 
+# FUNCTION TO DETECT EVENTS AND ACCORDINGLY DISPLAY THE REQUIRED ANNOTATION
 
 def hover(event):
     vis = annot.get_visible()
@@ -113,6 +145,7 @@ def hover(event):
         fig.canvas.draw_idle()
 
 
+# SET FUNCTION HOVER AS THE FUNCTION TO BE USED WHEN AN EVENT TAKES PLACE
 fig.canvas.mpl_connect("motion_notify_event", hover)
 
 plt.show()
